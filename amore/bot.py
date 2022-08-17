7b8779df8e946ae21140f2010cd429be0dfa3828
DS <studnikoff@gmail.com>
2022-08-17 15:46:20 +0300

    Small changes in job_queue and getting chat statisitcs
    fix(bot): bug with job_queue fixed. Now only one dailt job creates
    without start. Earlier every /start command from users created job
    and compliments were stacked.
    
    feature(bot): /chats_stat command for getting number of chats and
    their ids.

--- amore/bot.py
+++ amore/bot.py
@@ -22,6 +22,12 @@ dispatcher = updater.dispatcher
 
 def message_for_her(*args, **kwargs):
     bot = Bot(TOKEN)
+
+    chat_ids = get_chats()
+    if len(chat_ids) == 0:
+        return 0
+
+    
     gen = Generator(scale=0.3)
     try:
         cmpl = gen.choose_compliment()
@@ -42,7 +48,6 @@ def message_for_her(*args, **kwargs):
 
     msg = emoji.emojize(text, language='alias')
 
-    chat_ids = get_chats()
     for chat_id in chat_ids:
         bot.send_message(chat_id=chat_id, text=msg)
 
@@ -61,14 +66,13 @@ def start(update: Update, context: CallbackContext):
                          language='alias')
     context.bot.send_message(chat_id=chat.chat_id, 
                              text=text)
-    
-    logging.debug("Set schedule")
 
-    # Moscow: UTC+3:00, TODO: tzinfo
-    context.job_queue.run_daily(message_for_her, datetime.time(4, 0, 0, tzinfo=None)) # Time in UTC
-
-
-
+def chats_stat(update: Update, context: CallbackContext):
+    chat_id = update.effective_chat.id
+    chats = get_chats()
+    text = f"Всего чатов создано: {str(len(chats))}. Их id: {str(chats)}"
+    context.bot.send_message(chat_id=chat_id, 
+                             text=text)
 
 def send_answer(update: Update, context: CallbackContext):
     text = "К сожалению, я пока ещё не умею отвечать :cry: \nНо в будущем возможно научусь! :smile:"
@@ -77,11 +81,21 @@ def send_answer(update: Update, context: CallbackContext):
 
 def start_bot():
     logging.info("Bot start")
+    logging.info("start handler")
     start_handler = CommandHandler('start', start)
     dispatcher.add_handler(start_handler)
 
+    logging.info("chats_stat handler")
+    chats_stat_handler = CommandHandler('chats_stat', chats_stat)
+    dispatcher.add_handler(chats_stat_handler)
+
     send_answer_handler = MessageHandler(Filters.text & (~Filters.command), send_answer)
     dispatcher.add_handler(send_answer_handler)
+
+    job = updater.job_queue
+    logging.debug("Set schedule")
+    # Moscow: UTC+3:00, TODO: tzinfo
+    job.run_daily(message_for_her, datetime.time(4, 0, 0, tzinfo=None)) # Time in UTC
 
     updater.start_polling()
 
